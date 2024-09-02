44097ad80b2acfbe1c8830ffa9705cf36fd492d9
Coderyounes <younesanas2021@gmail.com>
2024-09-02 23:11:45 +0100

    affichage température

--- DAY01/CH5.c
+++ DAY01/CH5.c
@@ -1,1 +1,18 @@
-x
+#include <stdio.h>
+
+int main() {
+	int C;
+
+	printf("Entree la temperature: ");
+	scanf("%d", &C);
+
+	if ( C < 0) {
+		printf("Solide\n");
+	} else if ( 0 <= C && C < 100) {
+		printf("Liquide\n");
+	} else {
+		printf("Gaz\n");
+	}
+
+	return (0);
+}
