a8040ddc322ec1a67431a00fc5483c459f5feb47
Coderyounes <younesanas2021@gmail.com>
2024-09-02 20:51:37 +0100

    initial first Challange

---
+++ CH1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+	char firstName[100];
+
+	printf("Enter your First Name: ");
+	scanf("%s", firstName);
+	printf("Your Name is: %s\n", firstName);
+	return (0);
+}
